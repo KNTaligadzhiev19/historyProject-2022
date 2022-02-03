9d1c88b3f87d8b0e83137d21519372a2b2782166
Anton Ivanov <gvivanov19@codingburgas.bg>
2022-02-03 19:03:51 +0200

    Create Timeline menu #12

--- historyProject/historyProject/Front-End/CPP Files/startProgram.cpp
+++ historyProject/historyProject/Front-End/CPP Files/startProgram.cpp
@@ -1,4 +1,31 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+void switchPages(sf::RenderWindow& window, sf::Event& userEvent, sf::Texture& currentPageTexture)
+{
+    switch (userEvent.type)
+    {
+    case sf::Event::Closed:
+    {
+        window.close();
+        break;
+    }
+
+    case sf::Event::MouseButtonPressed:
+    {
+        if ((userEvent.mouseButton.x >= 170 && userEvent.mouseButton.x <= 625) &&
+            (userEvent.mouseButton.y >= 400 && userEvent.mouseButton.y <= 510))
+        {
+            std::cout << "Enter timeline menu \n";
+            currentPageTexture.loadFromFile("Images and fonts/Timeline menu.png");
+        }
+        break;
+    }
+
+    default:
+        break;
+    }
+}
 
 void startProgram()
 {
@@ -16,10 +43,7 @@ void startProgram()
         sf::Event userEvent;
         while (window.pollEvent(userEvent))
         {
-            if (userEvent.type == sf::Event::Closed)
-            {
-                window.close();
-            }
+            switchPages(window, userEvent, *currentPageTexture);
         }
 
         window.clear();
